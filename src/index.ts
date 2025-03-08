aa86ac186607520ba14341fb4fb5fc244ad17522
xayanide <xayanide@gmail.com>
2025-03-09 07:36:22 +0800

    fix: improve async function validation, error messages
    - fix: show dirPath with the error message when provided an invalid directory
    - refactor: types
    - refactor: rename variables for context clarity
    - refactor: sort variables

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import * as nodeUrl from "node:url";
 import * as nodeFsPromises from "node:fs/promises";
 import type { EventEmitter } from "node:events";
 import type { PathLike } from "node:fs";
-import type { EventExecute, EventHandler, EventHandlerKeys, EventHandlerModule, LoadEventHandlersOptions } from "./types.js";
+import type { EventExecute, EventHandlerModuleExport, EventHandlerModuleNamespace, EventHandlerKeys, LoadEventHandlersOptions } from "./types.js";
 
-const EVENT_EMITTER_ADD_LISTENER_METHOD_NAMES = ["on", "once", "addListener", "prependListener", "prependOnceListener"];
 const DEFAULT_EXPORT_NAME = "default";
+const EVENT_EMITTER_ADD_LISTENER_METHOD_NAMES = ["on", "once", "addListener", "prependListener", "prependOnceListener"];
+
 const DEFAULT_EVENT_HANDLER_KEY_NAMES = {
     name: "name",
     isOnce: "isOnce",
@@ -35,12 +36,8 @@ async function isValidDirectory(dirPath: PathLike) {
     }
 }
 
-function isAsyncFunction(fn: EventExecute) {
-    const isFunction = typeof fn === "function";
-    if (isFunction) {
-        return fn.constructor.name === "AsyncFunction";
-    }
-    return false;
+function isAsyncFunction(fn: unknown) {
+    return typeof fn === "function" && fn.constructor.name === "AsyncFunction";
 }
 
 function hasAddListenerMethods(object: EventEmitter): boolean {
@@ -86,22 +83,22 @@ function getAsyncAwareListener(executeMethod: EventExecute, listenerPrependedArg
 
 function bindEventListener(
     eventEmitterLike: EventEmitter,
-    eventHandler: EventHandler,
+    moduleExport: EventHandlerModuleExport,
     preferredEventHandlerKeys: EventHandlerKeys,
     listenerPrependedArgs: unknown[],
     fileUrlHref: string,
 ) {
     const { name: nameKeyName, isOnce: isOnceKeyName, isPrepend: isPrependKeyName, execute: executeKeyName } = preferredEventHandlerKeys;
-    if ((nameKeyName as "name") in eventHandler === false) {
+    if ((nameKeyName as "name") in moduleExport === false) {
         throw new Error(`Unable to find an exported property '${nameKeyName}'. Module: ${fileUrlHref}`);
     }
-    if ((executeKeyName as "execute") in eventHandler === false) {
+    if ((executeKeyName as "execute") in moduleExport === false) {
         throw new Error(`Unable to find an exported property '${executeKeyName}'. Module: ${fileUrlHref}`);
     }
-    const nameValue = String(eventHandler[nameKeyName as "name"]);
-    const isOnceValue = eventHandler[isOnceKeyName as "isOnce"];
-    const isPrependValue = eventHandler[isPrependKeyName as "isPrepend"];
-    const executeMethod = eventHandler[executeKeyName as "execute"];
+    const nameValue = String(moduleExport[nameKeyName as "name"]);
+    const isOnceValue = moduleExport[isOnceKeyName as "isOnce"];
+    const isPrependValue = moduleExport[isPrependKeyName as "isPrepend"];
+    const executeMethod = moduleExport[executeKeyName as "execute"];
     if (!nameValue || (typeof nameValue !== "string" && typeof nameValue !== "symbol")) {
         throw new Error(`Invalid value for key ${nameKeyName}: '${nameValue}'. Must be a non-empty string or symbol. Module: ${fileUrlHref}`);
     }
@@ -130,39 +127,42 @@ function bindEventListener(
     }
 }
 
-async function importEventHandler(fileUrlHref: string, exportType: string, preferredNamedExport: string) {
+async function importModule(fileUrlHref: string, exportType: string, preferredExportName: string) {
     const isNamedExportType = exportType === "named";
-    const importedModule: EventHandlerModule = await import(fileUrlHref);
-    if (isNamedExportType && preferredNamedExport === "*") {
-        const eventExports: EventHandler[] = [];
-        for (const exportName in importedModule) {
-            const eventHandler = importedModule[exportName];
-            if (!eventHandler || !Object.prototype.hasOwnProperty.call(importedModule, exportName) || exportName === DEFAULT_EXPORT_NAME) {
-                throw new Error(`Invalid event handler module. Must be a named export. Unable to verify named export '${exportName}'. Module: ${fileUrlHref}`);
+    const moduleNamespace: EventHandlerModuleNamespace = await import(fileUrlHref);
+    if (isNamedExportType && preferredExportName === "*") {
+        const moduleExports: EventHandlerModuleExport[] = [];
+        for (const exportName in moduleNamespace) {
+            const moduleExport = moduleNamespace[exportName];
+            if (!moduleExport || !Object.prototype.hasOwnProperty.call(moduleNamespace, exportName) || exportName === DEFAULT_EXPORT_NAME) {
+                console.error(
+                    `Invalid module. Must be a named export called '${preferredExportName}'. Unable to verify named export '${exportName}'. Module: ${fileUrlHref}`,
+                );
+                continue;
             }
-            eventExports.push(eventHandler);
+            moduleExports.push(moduleExport);
         }
-        return eventExports;
+        return moduleExports;
     } else {
-        const exportName = isNamedExportType ? preferredNamedExport : DEFAULT_EXPORT_NAME;
-        const eventHandler = importedModule[exportName];
-        if (!eventHandler || !Object.prototype.hasOwnProperty.call(importedModule, exportName)) {
+        const exportName = isNamedExportType ? preferredExportName : DEFAULT_EXPORT_NAME;
+        const moduleExport = moduleNamespace[exportName];
+        if (!moduleExport || !Object.prototype.hasOwnProperty.call(moduleNamespace, exportName)) {
             throw new Error(
                 isNamedExportType
-                    ? `Invalid event handler module. Must be a named export. ${
-                          exportName === DEFAULT_NAMED_EXPORT ? "Unable to verify named export" : "Unable to verify preferred named export"
+                    ? `Invalid module. Must be a named export called '${preferredExportName}'. ${
+                          exportName === DEFAULT_EXPORT_NAME ? "Unable to verify named export" : "Unable to verify preferred export name"
                       } '${exportName}'. Module: ${fileUrlHref}`
-                    : `Invalid event handler module. Must be a default export. Unable to verify default export '${exportName}'. Module: ${fileUrlHref}`,
+                    : `Invalid module. Must be a default export. Unable to verify default export '${exportName}'. Module: ${fileUrlHref}`,
             );
         }
-        return [eventHandler];
+        return [moduleExport];
     }
 }
 
 async function loadEventHandlers(dirPath: string, eventEmitterLike: EventEmitter, options?: LoadEventHandlersOptions) {
     const isValidDir = await isValidDirectory(dirPath);
     if (!isValidDir) {
-        throw new Error("Invalid event handler directory path. Must be an existent directory.");
+        throw new Error(`Invalid event handler directory path: ${dirPath}. Must be an existent directory.`);
     }
     if (!hasAddListenerMethods(eventEmitterLike)) {
         throw new Error("Invalid eventEmitterLike instance. Must have EventEmitter methods.");
@@ -187,10 +187,10 @@ async function loadEventHandlers(dirPath: string, eventEmitterLike: EventEmitter
     if (!executeKeyName || typeof executeKeyName !== "string") {
         throw new Error(`Invalid value for preferredEventHandlerKeys execute: '${executeKeyName}'. Must be a non-empty string.`);
     }
-    if (!importMode || typeof importMode !== "string" || !DEFAULT_IMPORT_MODES.includes(importMode)) {
+    if (!importMode || !DEFAULT_IMPORT_MODES.includes(importMode)) {
         throw new Error(`Invalid import mode: ${importMode}. Must be one of string: ${DEFAULT_IMPORT_MODES.join(", ")}`);
     }
-    if (!exportType || typeof exportType !== "string" || !DEFAULT_EXPORT_TYPES.includes(exportType)) {
+    if (!exportType || !DEFAULT_EXPORT_TYPES.includes(exportType)) {
         throw new Error(`Invalid export type: ${exportType}. Must be one of string: ${DEFAULT_EXPORT_TYPES.join(", ")}`);
     }
     if (!preferredNamedExport || typeof preferredNamedExport !== "string") {
@@ -201,14 +201,14 @@ async function loadEventHandlers(dirPath: string, eventEmitterLike: EventEmitter
         throw new Error(`Invalid event handler files. No event handler files found in directory: ${dirPath}`);
     }
     async function loadEventHandler(file: string) {
-        if (!file.endsWith(".js") && !file.endsWith(".ts")) {
+        if (!file.endsWith(".js") && !file.endsWith(".ts") && !file.endsWith(".mjs") && !file.endsWith("cjs")) {
             return;
         }
         const filePath = nodePath.join(dirPath, file);
         const fileUrlHref = nodeUrl.pathToFileURL(filePath).href;
-        const eventExports = await importEventHandler(fileUrlHref, exportType as string, preferredNamedExport as string);
-        for (const eventHandler of eventExports) {
-            bindEventListener(eventEmitterLike, eventHandler, preferredEventHandlerKeys, listenerPrependedArgs as unknown[], fileUrlHref);
+        const moduleExports = await importModule(fileUrlHref, exportType as string, preferredNamedExport as string);
+        for (const moduleExport of moduleExports) {
+            bindEventListener(eventEmitterLike, moduleExport, preferredEventHandlerKeys, listenerPrependedArgs as unknown[], fileUrlHref);
         }
     }
     if (importMode === "parallel") {
